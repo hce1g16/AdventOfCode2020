5678ae0778ba15bf86cb735e345a5e48bea0dc27
Harry Everett <github@harryeverett.com>
2020-12-03 00:29:45 +0000

    Add day 1 solutions

---
+++ Day1/day1a.hs
@@ -0,0 +1,23 @@
+import System.IO
+
+main = do
+     g <- readFile "Day1/input"
+     let l = lines(g)
+     let m = map (read::String->Int) l
+     let answer = checkExpense m 2020 
+     print answer
+
+checkExpense :: [Int] -> Int -> Int
+
+checkExpense (x:xs) target 
+    | result /= -1 = result
+    | otherwise = checkExpense xs target
+    where result = checkSum x xs target
+
+checkSum :: Int -> [Int] -> Int -> Int
+
+checkSum x (y:ys) target
+    | x + y == target = x*y
+    | otherwise       = checkSum x ys target
+
+checkSum x [] target = -1
---
+++ Day1/day1b.hs
@@ -0,0 +1,36 @@
+import System.IO
+import Data.List
+
+main = do
+     g <- readFile "Day1/input"
+     let l = lines(g)
+     let m = map (read::String->Int) l
+     let answer = checkExpense' m 2020 
+     print answer
+
+checkExpense' :: [Int] -> Int -> Int
+
+checkExpense' (x:xs) target 
+    | result /= -1 = result * x
+    | otherwise    = checkExpense' xs target
+    where remainder = target - x
+          result    = checkExpense xs remainder
+
+checkExpense' [] _ = -1
+
+checkExpense :: [Int] -> Int -> Int
+
+checkExpense (x:xs) target 
+    | result /= -1 = result
+    | otherwise    = checkExpense xs target
+    where result = checkSum x xs target
+
+checkExpense [] _ = -1
+
+checkSum :: Int -> [Int] -> Int -> Int
+
+checkSum x (y:ys) target
+    | x + y == target = x*y
+    | otherwise       = checkSum x ys target
+
+checkSum x [] target = -1
