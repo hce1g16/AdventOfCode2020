a6c1aa9911327c5a239d4c5c61cde68cbf6f1379
Harry Everett <github@harryeverett.com>
2020-12-03 01:05:52 +0000

    Tidy day1 code

--- day1/day1a.hs
+++ day1/day1a.hs
@@ -1,23 +1,20 @@
-import System.IO
-
+main :: IO ()
 main = do
-     g <- readFile "day1/input"
-     let l = lines(g)
-     let m = map (read::String->Int) l
-     let answer = checkExpense m 2020 
-     print answer
+  g <- readFile "day1/input"
+  let l = lines g
+  let m = map (read :: String -> Int) l
+  let answer = checkExpense m 2020
+  print answer
 
 checkExpense :: [Int] -> Int -> Int
-
-checkExpense (x:xs) target 
-    | result /= -1 = result
-    | otherwise = checkExpense xs target
-    where result = checkSum x xs target
+checkExpense (x : xs) target
+  | result /= -1 = result
+  | otherwise = checkExpense xs target
+  where
+    result = checkSum x xs target
 
 checkSum :: Int -> [Int] -> Int -> Int
-
-checkSum x (y:ys) target
-    | x + y == target = x*y
-    | otherwise       = checkSum x ys target
-
-checkSum x [] target = -1
+checkSum x (y : ys) target
+  | x + y == target = x * y
+  | otherwise = checkSum x ys target
+checkSum _ [] _ = -1
--- day1/day1b.hs
+++ day1/day1b.hs
@@ -1,35 +1,30 @@
-import System.IO
-
+main :: IO ()
 main = do
-     g <- readFile "day1/input"
-     let l = lines(g)
-     let m = map (read::String->Int) l
-     let answer = checkExpense' m 2020 
-     print answer
+  g <- readFile "day1/input"
+  let l = lines g
+  let m = map (read :: String -> Int) l
+  let answer = checkExpense' m 2020
+  print answer
 
 checkExpense' :: [Int] -> Int -> Int
-
-checkExpense' (x:xs) target 
-    | result /= -1 = result * x
-    | otherwise    = checkExpense' xs target
-    where remainder = target - x
-          result    = checkExpense xs remainder
-
+checkExpense' (x : xs) target
+  | result /= -1 = result * x
+  | otherwise = checkExpense' xs target
+  where
+    remainder = target - x
+    result = checkExpense xs remainder
 checkExpense' [] _ = -1
 
 checkExpense :: [Int] -> Int -> Int
-
-checkExpense (x:xs) target 
-    | result /= -1 = result
-    | otherwise    = checkExpense xs target
-    where result = checkSum x xs target
-
+checkExpense (x : xs) target
+  | result /= -1 = result
+  | otherwise = checkExpense xs target
+  where
+    result = checkSum x xs target
 checkExpense [] _ = -1
 
 checkSum :: Int -> [Int] -> Int -> Int
-
-checkSum x (y:ys) target
-    | x + y == target = x*y
-    | otherwise       = checkSum x ys target
-
-checkSum x [] target = -1
+checkSum x (y : ys) target
+  | x + y == target = x * y
+  | otherwise = checkSum x ys target
+checkSum _ [] _ = -1
