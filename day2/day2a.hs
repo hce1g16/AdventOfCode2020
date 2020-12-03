3e23518f0293a099de23bc086c1e55abcb887016
Harry Everett <github@harryeverett.com>
2020-12-03 01:14:06 +0000

    CLean up solutions

--- day2/day2a.hs
+++ day2/day2a.hs
@@ -1,21 +1,21 @@
-import System.IO
 import Text.Regex.TDFA
 
+main :: IO ()
 main = do
-     g <- readFile "day2/input"
-     let l = lines(g)
-     -- parse the input and pass each value of it to checkPassword 
-     let parsed = [(i =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: (String, String, String, [String])) | i <- l] 
-     let matches = [checkPassword pw char (read low :: Int) (read high :: Int) | (_, _, _, low:high:(char:_):pw:_) <- parsed]
-     let result = sum matches     
-     print result
+  g <- readFile "day2/input"
+  let l = lines g
+  -- parse the input and pass args from it to checkPassword
+  let parsed = [(i =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: (String, String, String, [String])) | i <- l]
+  let matches = [checkPassword pw char (read low :: Int) (read high :: Int) | (_, _, _, low : high : (char : _) : pw : _) <- parsed]
+  let result = sum matches
+  print result
 
 -- Checks that a password has between low and high occurances of char
 checkPassword :: String -> Char -> Int -> Int -> Int
-
 checkPassword pw char low high
-    | (freq <= high && freq >= low) = 1
-    | otherwise                     = 0
-    where freq = frequency(char,pw)
+  | freq <= high && freq >= low = 1
+  | otherwise = 0
+  where
+    freq = frequency (char, pw)
 
-frequency(n, (x:xs)) = length([a | a <- (x:xs), a==n])
+frequency (n, x : xs) = length ([a | a <- x : xs, a == n])
--- day2/day2b.hs
+++ day2/day2b.hs
@@ -1,26 +1,24 @@
-import System.IO
 import Text.Regex.TDFA
 
+main :: IO ()
 main = do
-     g <- readFile "day2/input"
-     let l = lines(g)
-     -- parse the input and pass each value of it to checkPassword 
-     let parsed = [(i =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: (String, String, String, [String])) | i <- l] 
-     let matches = [checkPassword pw char (read low :: Int) (read high :: Int) | (_, _, _, low:high:(char:_):pw:_) <- parsed]
-     let result = sum matches     
-     print result
+  g <- readFile "day2/input"
+  let l = lines g -- parse the input and pass args from it to checkPassword
+  let parsed = [(i =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: (String, String, String, [String])) | i <- l]
+  let matches = [checkPassword pw char (read low :: Int) (read high :: Int) | (_, _, _, low : high : (char : _) : pw : _) <- parsed]
+  let result = sum matches
+  print result
 
 -- Checks that a password has char in one of pos p1 or p1 but not both
 checkPassword :: String -> Char -> Int -> Int -> Int
-
 checkPassword pw char p1 p2
-    | xor (ele1 == char) (ele2 == char) = 1
-    | otherwise                         = 0
-    where ele1 = pw !! (p1-1)
-          ele2 = pw !! (p2-1)
+  | xor (ele1 == char) (ele2 == char) = 1
+  | otherwise = 0
+  where
+    ele1 = pw !! (p1 - 1)
+    ele2 = pw !! (p2 - 1)
 
 xor :: Bool -> Bool -> Bool
-
 xor a b
-    | (a == b) = False
-    | otherwise = True
+  | a == b = False
+  | otherwise = True
